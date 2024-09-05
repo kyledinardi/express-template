8dce03ed0c3d76e97448ed3c5652b6696f88a42c
kyledinardi <94251418+kyledinardi@users.noreply.github.com>
2024-09-05 13:41:12 -0400

    Add prisma and remove mongoose and middleware

--- models/user.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const UserSchema = new Schema({
-  username: { type: String, required: true },
-  password: { type: String, required: true },
-  isAdmin: { type: Boolean },
-});
-
-module.exports = model('User', UserSchema);
